ded31e74b4abe3df8d27975797877ca643e63aed
DSchroederOSU <schrodan@oregonstate.edu>
2017-12-13 22:54:23 -0800

    blocks stuff

--- Source/public/js/core.js
+++ Source/public/js/core.js
@@ -4,11 +4,23 @@ myApp.config(function($routeProvider, $locationProvider) {
         .when("/", {
             templateUrl : "../views/home.html"
         })
+        .when("/dashboards", {
+            templateUrl : "../views/dashboards.html"
+        })
+        .when("/editblock", {
+            templateUrl : "../views/block/edit-block.html"
+        })
+        .when("/deleteblock", {
+            templateUrl : "../views/block/delete-block.html"
+        })
+        .when("/blocks", {
+            templateUrl : "../views/blocks.html"
+        })
         .when("/createBlock", {
             templateUrl : "../views/create-block.html"
         })
         .when("/viewBlocks", {
             templateUrl : "../views/view-blocks.html"
         });
-    $locationProvider.html5Mode(true);
+    //$locationProvider.html5Mode(true);   //this is what is breaking page reload
 });
